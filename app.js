06a7658bac699b76e7484809d332ae76ab6e06e2
drummachina <drummachina@gmail.com>
2022-12-11 19:57:54 +0100

    re-upload files

--- app.js
+++ app.js
@@ -1,53 +1,88 @@
+import chalk from 'chalk';
+import JSON5 from 'json5';
+import { readFile } from 'fs/promises';
+const data = JSON5.parse(
+  await readFile(
+    new URL('./data.json5', import.meta.url)
+  )
+);
+// import utils from './utils.js';
+
 // TODO [2022-11-20] Dodać nazwy polskich pór roku do tablicy seasonsArr
 // TODO [2022-11-20] Dodać obsługę parametru "s-pl", patrz opis parametru poniżej
 
-const monthsArr = [
-    { pl: 'Styczeń',    en: 'January'   }, // index: 0,   Zima
-    { pl: 'Luty',       en: 'February'  }, // index: 1,   Zima
-    { pl: 'Marzec'                      }, // index: 2,   Zima, Wiosna,
-    { pl: 'Kwieciń'                     }, // index: 3,   Wiosna
-    { pl: 'maj'                         }, // index: 4,   Wiosna
-    { pl: 'czerwiec'                    }, // index: 5,   Wiosna, Lato
-    {},                                    // index: 6    Lato
-    {},                                    // index: 7    Lato
-    {},                                    // index: 8    Lato, Jesień
-    {                   en: 'October'   }, // index: 9    Jesiń
-    {},                                    // index: 10   Jesiń
-    { pl: 'Grudzień'                    }  // index: 11,  Jesiń, Zima
-];
-
-const seasonsArr = [
-    { pl: 'Wiosna',     en: 'Spring', monthsArrIdxArr: [ 2, 3, 4, 5 ] },
-    {                                 monthsArrIdxArr: [ 5, 6  ] },
-    {},
-    {}
-];
 
 if (process.argv.length < 3) { // no params provided
-  console.log('No params, exit.')
+  console.log(chalk.red('No params, exit.'))
   process.exit();
 }
 
 const param = process.argv[2]; // only 1 param support by now
-  
-switch (param) {
-  
-  case 'test':
-    console.log(`This is index: 0 - ${monthsArr[0].pl}`);
-    console.log(`This is index: 3 - ${monthsArr[3]}`);
-    break;
-  
-  case 'm-en': // months-english, lista miesięcy po angielsku
-      monthsArr.forEach((elem, index) => {
-        console.log(elem.en);
-      });
-      break;
+
+const paramToFunction = {
+  'help': {
+    description: 'List supported options.',
+    actionFunc: () => {
       
-  case 'm-pl': // months-polish, lista miesięcy po polsku
-      monthsArr.forEach((elem) => {
+       console.log(chalk.magenta(
+'Node.js Command line app, displays months and seasons list in English and Polish language.\n\
+How to run: node app.js <param_required>, ex: node app.js m-pl\n\
+Date: 2022-11-20\n\
+Author: drummachina@gmail.com\n\
+Purpose: For months / season training, for zombolol2x\n'));
+
+      console.log('List of support options:');
+      
+      for (let key in paramToFunction) {
+        if (key === 'help') {
+          continue;
+        }
+        console.log(chalk.magenta(` ${key}: ${paramToFunction[key].description}`));
+      }
+      
+      console.log();
+    }
+  },
+  'm-en': {
+    description: 'months-english, lista miesięcy po angielsku',
+    actionFunc: () => {
+      data.monthsArr.forEach((elem, index) => {
+        let out;
+        if (!elem.en) {
+          out = chalk.gray(elem.en);
+        } else {
+          out = chalk.yellow(elem.en);
+        }
+        console.log(out);
+      });
+    }
+  },
+  'm-pl': {
+    description: 'months-polish, lista miesięcy po polsku',
+    actionFunc: () => {
+      data.monthsArr.forEach((elem) => {
         console.log(elem.pl);
       });
-      break;
+    }
+  },
+  'test': {
+    description: 'Test action',
+    actionFunc: () => {
+      console.log(`This is index: 0 - ${data.monthsArr[0].pl}`);
+      console.log(`This is index: 3 - ${data.monthsArr[3]}`);
+    }
+  }
+};
+
+if (!paramToFunction[param]) {
+  console.log(chalk.red(`Param: "${param}" is not recognized, exit.`));
+  process.exit();
+} else {
+  paramToFunction[param].actionFunc();
+}
+
+
+
       
   // 's-pl'         - seasons-polish, pory roku po polsku
   // 's-en'         - seasons-english, pory roku po angielsku
@@ -59,8 +94,3 @@ switch (param) {
   // 'm-summer-pl'  - months-summer-polish, miesiące lato po polsku
   // 'm-autom-pl'   - months-autom-polish, miesiące jesień po polsku
   // 'm-winter-pl'  - months-winter-polish, miesiące zima po polsku
-      
-  default:
-    console.log(`Param: "${param}" is not recognized, exit.`);
-    process.exit();
-}
